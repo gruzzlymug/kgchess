b8844d5eb81cd45f331730cca4a3b6d8fd1fa71c
gruzzlymug <kevin@gruzzlymug.com>
2016-07-09 04:13:31 +0000

    improve migration

--- db/migrate/20160708223438_add_fks_to_piece.rb
+++ db/migrate/20160708223438_add_fks_to_piece.rb
@@ -1,11 +1,15 @@
 class AddFksToPiece < ActiveRecord::Migration
   def up
-    add_column :pieces, :game_id, :integer
-    add_column :pieces, :player_id, :integer
+    change_table :pieces, bulk: true do |t|
+      t.belongs_to :game
+      t.belongs_to :player
+    end
   end
 
   def down
-    remove_column :pieces, :game_id
-    remove_column :pieces, :player_id
+    change_table :pieces, bulk: true do |t|
+      t.remove :game_id
+      t.remove :player_id
+    end
   end
 end
