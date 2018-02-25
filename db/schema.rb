f22d621049f07201c1a4f7f5ec406af8b47550e8
Kevin Guran <kevin@gruzzlymug.com>
2018-02-25 10:29:13 -0600

    add selected piece to game (#28)
    * add selected piece to game
    
    * fix whitespace

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,50 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708223438) do
+ActiveRecord::Schema.define(version: 20180225145232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "games", force: :cascade do |t|
-    t.integer  "white_player_id"
-    t.integer  "black_player_id"
-    t.string   "name"
-    t.string   "status"
-    t.integer  "turn"
-    t.integer  "winner_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-  create_table "pieces", force: :cascade do |t|
-    t.string   "type"
-    t.integer  "pos_x"
-    t.integer  "pos_y"
-    t.integer  "moves"
-    t.string   "status"
+  create_table "games", id: :serial, force: :cascade do |t|
+    t.integer "white_player_id"
+    t.integer "black_player_id"
+    t.string "name"
+    t.string "status"
+    t.integer "turn"
+    t.integer "selected_piece_id"
+    t.integer "winner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "game_id"
-    t.integer  "player_id"
   end
 
-  create_table "players", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "pieces", id: :serial, force: :cascade do |t|
+    t.string "type"
+    t.integer "pos_x"
+    t.integer "pos_y"
+    t.integer "moves"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "game_id"
+    t.integer "player_id"
+  end
+
+  create_table "players", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_players_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
   end
-
-  add_index "players", ["email"], name: "index_players_on_email", unique: true, using: :btree
-  add_index "players", ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true, using: :btree
 
 end
