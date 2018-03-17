5aaa1dbf47a60c3fc9f4f6106b5bab849960de6b
Kevin Guran <kevin@gruzzlymug.com>
2018-03-17 17:53:03 -0500

    add basic controller test (#42)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'devise'
 require 'support/factory_bot'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -55,4 +56,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
